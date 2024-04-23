55347ed7d472e58caf753c1b0be022d6ace83795
Ian Moore <ianswimoore@gmail.com>
2024-04-22 20:29:11 -0600

    changed api constant back to production

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,5 @@
 class ApiConstants {
-  // static String baseUrl = 'https://upark-capstone-web-server.web.app';
-  static String baseUrl = 'http://10.0.2.2:8080';
+  static String baseUrl = 'https://upark-capstone-web-server.web.app';
   static String lotsEndpoint = '/lots';
   static String reportsEndpoint = '/reports';
   static String buildingsEndpoint = '/buildings';
